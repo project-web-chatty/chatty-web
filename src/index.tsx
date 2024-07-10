3d9e90c7c8d5c57d1baadc16670044bea6be31e6
전하윤 <kjklove0701@gmail.com>
2024-07-11 05:47:09 +0900

    Fix: 충돌 수정(해결), React.StrictMode -> BrowserRouter 수정
    App.tsx에서 pull 시 생긴 충돌 수정(해결),
    react-router-dom 라이브러리 설치 후 React.StrictMode -> BrowserRouter 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "./styles/index.css";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
->>>>>>> df41f69e30786bb923c06013447fa17dd2f10a1f
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
   <BrowserRouter>
     <App />
-<<<<<<< HEAD
-  </React.StrictMode>,
-=======
   </BrowserRouter>
->>>>>>> df41f69e30786bb923c06013447fa17dd2f10a1f
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
