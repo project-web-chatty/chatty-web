e09333096ff408fb444df7ab5127d417c1a432b3
TaekyungAn <atk0426@naver.com>
2024-07-10 20:08:42 +0900

    Chore: ESLint 및 Prettier 설정

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 frontend repository for chatty web service team projects
 
 ## 프로젝트 실행 방법
+
 npm start
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      body: '#2F3645',
-      white: '#FFFFFF',
-      black: '#000000',
-      orange: '#DD5746',
-      purple: '#6562F5',
+      body: "#2F3645",
+      white: "#FFFFFF",
+      black: "#000000",
+      orange: "#DD5746",
+      purple: "#6562F5",
     },
     fontFamily: {},
     extend: {},
