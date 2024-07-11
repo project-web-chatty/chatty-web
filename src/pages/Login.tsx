041f9a41eff25d0a75f96e02681c1553f2b1f30b
TaekyungAn <atk0426@naver.com>
2024-07-12 00:18:49 +0900

    Design: 로그인 화면 CSS 수정
    간단한 반응형 추가 및 소셜로그인 아이콘 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,51 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo_google from "../assets/image/logo_google.png";
+import logo_github from "../assets/image/logo_github.png";
 
 function Login() {
   return (
-    <div className="h-full p-20">
-      <div className="flex items-center justify-center">
-        <p className="text-3xl font-bold text-white">WELCOME TO&nbsp;</p>
-        <p className="text-3xl font-bold text-orange">CHATTY</p>
-      </div>
-      <div className="flex items-center justify-center pt-14">
-        <p className="text-xl font-bold text-white">SIGN UP</p>
-      </div>
-      <div className="items-center pt-10 mx-96">
-        <p className="text-l font-bold text-white">ID</p>
-        <div className="flex items-center mt-2">
-          <input
-            type="text"
-            className="border-2 border-white w-full bg-body py-2 px-5 rounded-md text-white focus:outline-none"
-            placeholder="아이디를 입력해주세요."
-          />
+    <div className="flex h-screen items-center justify-center p-20">
+      <div className="flex w-4/5 flex-col items-center justify-center">
+        <div>
+          <span className="hidden text-4xl font-bold text-white md:inline">WELCOME TO&nbsp;</span>
+          <span className="text-5xl font-bold text-orange">CHATTY</span>
         </div>
-      </div>
-      <div className="items-center pt-5 mx-96">
-        <p className="text-l font-bold text-white">PASSWORD</p>
-        <div className="flex items-center mt-2">
-          <input
-            type="text"
-            className="border-2 border-white w-full bg-body py-2 px-5 rounded-md text-white focus:outline-none"
-            placeholder="비밀번호를 입력해주세요."
-          />
+        <div className="w-full min-w-80 max-w-96 pt-10">
+          <div>
+            <p className="text-l font-bold text-white">ID</p>
+            <div className="mt-2 flex items-center">
+              <input
+                type="text"
+                className="h-12 w-full rounded-md border-2 border-white bg-body px-5 text-white focus:outline-none"
+                placeholder="아이디를 입력해주세요."
+              />
+            </div>
+          </div>
+          <div className="items-center pt-5">
+            <p className="text-l font-bold text-white">PASSWORD</p>
+            <div className="mt-2 flex items-center">
+              <input
+                type="text"
+                className="h-12 w-full rounded-md border-2 border-white bg-body px-5 text-white focus:outline-none"
+                placeholder="비밀번호를 입력해주세요."
+              />
+            </div>
+          </div>
+          <div className="mt-2 flex items-center">
+            <a href="#" className="text-xs text-purple">
+              비밀번호를 잊어버렸나요?
+            </a>
+          </div>
+          <div className="mt-7 flex items-center justify-center">
+            <button type="button" className="h-12 w-full rounded-lg bg-black p-2 text-white">
+              로그인
+            </button>
+          </div>
+          <div className="mt-2 flex items-center justify-center">
+            <p className="text-xs text-gray">webChatty를 처음 사용하시나요?</p>
+            <Link to="/signup">
+              <p className="pl-3 text-xs text-purple">회원가입</p>
+            </Link>
+          </div>
         </div>
-        <div className="flex items-center mt-2">
-          <p className="text-xs text-purple">비밀번호를 잊어버렸나요?</p>
-        </div>
-        <div className="flex items-center justify-center mt-10">
-          <button
-            type="button"
-            className="bg-black w-full p-2 rounded-lg text-white"
-          >
-            로그인
-          </button>
-        </div>
-        <div className="flex items-center justify-center mt-2">
-          <p className="text-xs text-gray">webChatty를 처음 사용하시나요?</p>
-          <Link to="/signup">
-            <p className="text-xs text-purple pl-3">회원가입</p>
-          </Link>
+        <div className="flex w-full min-w-80 flex-col items-center justify-center">
+          <hr className="my-8 h-px w-full max-w-96 border-0 bg-white" />{" "}
+          <div className="flex gap-20">
+            <a href="#">
+              <img src={logo_google} className="h-10 w-10" />
+            </a>
+            <a href="#">
+              <img src={logo_github} className="h-10 w-10" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
