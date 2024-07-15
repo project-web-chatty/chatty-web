9ace3cee2243c4cc9ef9ab9cde05ed0691dd9882
TaekyungAn <atk0426@naver.com>
2024-07-15 21:38:22 +0900

    Style: 첫페이지 워크스페이스 부분 컴포넌트로 분리

--- src/components/GroupIcon.tsx
+++ src/components/GroupIcon.tsx
@@ -1,15 +1,39 @@
-interface IGroupIcon {
+import icon_pencil from "../assets/icon/icon_pencil.png";
+import logo_coupang from "../assets/logo/logo_coupang.png";
+import logo_toss from "../assets/logo/logo_toss.png";
+import { Link } from "react-router-dom";
+
+export interface IGroupIcon {
   icon: string;
   text: string;
 }
 
 const GroupIcon = ({ icon, text }: IGroupIcon) => (
-  <div className="flex flex-col items-center justify-self-auto">
+  <Link
+    to={"/newworkspace"}
+    className="flex flex-col items-center justify-self-auto"
+  >
     <div className="flex items-center justify-center w-[80px] h-[80px] bg-white rounded-3xl">
       <img src={icon} />
     </div>
     <p className="text-white text-xs font-semibold mt-2">{text}</p>
-  </div>
+  </Link>
 );
 
-export default GroupIcon;
+const workspaces: IGroupIcon[] = [
+  { icon: logo_coupang, text: "쿠팡" },
+  { icon: logo_toss, text: "토스팀" },
+];
+
+const GroupMenu = () => {
+  return (
+    <div className="flex flex-wrap justify-evenly items-center h-full w-full max-w-[600px] gap-5 pt-8 pb-8">
+      {workspaces.map((item, index) => (
+        <GroupIcon key={index} icon={item.icon} text={item.text} />
+      ))}
+      <GroupIcon icon={icon_pencil} text="새 그룹 만들기" />
+    </div>
+  );
+};
+
+export default GroupMenu;
--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,46 +1,39 @@
-import icon_pencil from "../assets/icon/icon_pencil.png";
-import GroupIcon from "../components/GroupIcon";
+import GroupMenu from "../components/GroupIcon";
+import NewWorkspace from "../components/NewWorkspace";
 
-function FirstPage() {
+function FirstPage({ name = "이동현" }: any) {
   const newmember = true;
-  if (newmember || true) {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        <div className="flex w-4/5 h-full p-20 flex-col items-center justify-center">
-          <div className="h-10 mb-5">
-            <span className="hidden text-3xl font-bold text-white md:inline">
-              이동현 님의&nbsp;
-            </span>
-            <span className="text-3xl font-bold text-orange">
-              워크 스페이스
-            </span>
-          </div>
-          <div className="flex flex-wrap justify-evenly items-center h-full w-full max-w-[600px] gap-5 pt-8 pb-8">
-            {Array.from({ length: 1 }).map((_, index) => (
-              <GroupIcon key={index} icon={icon_pencil} text="새 그룹 만들기" />
-            ))}
-          </div>
 
-          <div className="flex flex-col items-center justify-center w-full min-w-80">
-            <p className="text-xl font-bold text-white p-8">초대 코드 입력</p>
-            <div className="flex items-center justify-center w-full max-w-md rounded-md border-2 border-black bg-white pl-5 pr-2">
-              <input
-                type="text"
-                className="h-10 w-full focus:outline-none"
-                placeholder="https://webChatty/~"
-              />
-              <button
-                type="button"
-                className="min-w-20 rounded-md bg-black p-1 text-white"
-                style={{ width: 80, marginLeft: 10 }}
-              >
-                참여
-              </button>
-            </div>
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex w-4/5 h-full p-20 flex-col items-center justify-center">
+        <h1 className="h-10 mb-5">
+          <span className="hidden text-3xl font-bold text-white md:inline">
+            {name} 님의&nbsp;
+          </span>
+          <span className="text-3xl font-bold text-orange">워크 스페이스</span>
+        </h1>
+        {/* <GroupMenu /> */}
+        <NewWorkspace />
+        <div className="flex flex-col items-center justify-center w-full min-w-80">
+          <p className="text-xl font-bold text-white p-8">초대 코드 입력</p>
+          <div className="flex items-center justify-center w-full max-w-md rounded-md border-2 border-black bg-white pl-5 pr-2">
+            <input
+              type="text"
+              className="h-10 w-full focus:outline-none"
+              placeholder="https://webChatty/~"
+            />
+            <button
+              type="button"
+              className="min-w-20 rounded-md bg-black p-1 text-white"
+              style={{ width: 80, marginLeft: 10 }}
+            >
+              참여
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default FirstPage;
