3628428928e82cba32b5c8f5e40a7844c5cc6622
TaekyungAn <atk0426@naver.com>
2024-07-19 00:48:02 +0900

    Feat: 새워크스페이스 생성전 미리보기 기능 구현중
    생성하기 버튼 hover시 나타나는 금지아이콘이 사용자 친화적이지 못하다고 생각하여 변경예정
    업로드된 로고 없을 시 기본 로고 제공하여야 함
    소개글을 필수로 지정할지 고민중
    미리보기 버튼 UI변경예정

--- src/components/WorkspaceForm.tsx
+++ src/components/WorkspaceForm.tsx
@@ -12,6 +12,7 @@ const WorkspaceForm: React.FC<WorkspaceFormProps> = ({ closeModal }) => {
   const [workspaceName, setWorkspaceName] = useState<string>("");
   const [workspaceDescription, setWorkspaceDescription] = useState<string>("");
   const [workspaceIcon, setWorkspaceIcon] = useState<string>(icon_upload);
+  const [isPreviewOpen, setIsPreviewOpen] = useState<boolean>(false);
   const dispatch = useDispatch();
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,6 +36,10 @@ const WorkspaceForm: React.FC<WorkspaceFormProps> = ({ closeModal }) => {
   };
 
   const handleFormSubmit = () => {
+    setIsPreviewOpen(true);
+  };
+
+  const handleConfirmSubmit = () => {
     if (workspaceName && workspaceDescription) {
       const newWorkspace = {
         icon: workspaceIcon,
@@ -46,65 +51,103 @@ const WorkspaceForm: React.FC<WorkspaceFormProps> = ({ closeModal }) => {
     }
   };
 
+  const handleCancelSubmit = () => {
+    setIsPreviewOpen(false);
+  };
+
   return (
-    <div className="flex justify-center items-center max-w-[600px] min-w-[400px] gap-5 bg-white rounded-xl">
-      <div className="flex flex-col justify-between items-center gap-5 m-5 w-full">
-        <div className="flex justify-between items-center w-full mb-5 relative">
-          <h2 className="text-2xl">새 워크 스페이스 만들기</h2>
-          <img
-            className="w-6 h-6 absolute -top-5 -right-5 cursor-pointer"
-            alt="닫기 아이콘"
-            src={icon_close}
-            onClick={closeModal}
-          />
-        </div>
-        <div className="w-full mb-3">
-          <p className="mb-2">워크스페이스 이름</p>
-          <input
-            type="text"
-            className="h-10 w-full focus:outline-none max-w-md rounded-md border-2 border-black p-5"
-            placeholder="이름을 정해주세요."
-            value={workspaceName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div className="w-full mb-5">
-          <p className="mb-2">소개</p>
-          <input
-            type="text"
-            className="h-10 w-full focus:outline-none max-w-md rounded-md border-2 border-black p-5"
-            placeholder="소개글을 작성해주세요."
-            value={workspaceDescription}
-            onChange={handleDescriptionChange}
-          />
-        </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex justify-center items-center">
-            <p className="mr-2">로고 업로드</p>
-            <input
-              type="file"
-              onChange={handleIconChange}
-              className="hidden"
-              id="icon-upload"
+    <>
+      <div className="flex justify-center items-center max-w-[600px] min-w-[400px] gap-5 bg-white rounded-xl">
+        <div className="flex flex-col justify-between items-center gap-5 m-5 w-full">
+          <div className="flex justify-between items-center w-full mb-5 relative">
+            <h2 className="text-2xl">새 워크 스페이스 만들기</h2>
+            <img
+              className="w-6 h-6 absolute -top-5 -right-5 cursor-pointer"
+              alt="닫기 아이콘"
+              src={icon_close}
+              onClick={closeModal}
             />
-            <label htmlFor="icon-upload">
-              <img
-                className="w-6 cursor-pointer"
-                alt="업로드아이콘"
-                src={workspaceIcon}
-              />
-            </label>
           </div>
-          <button
-            type="button"
-            className="min-w-20 rounded-md bg-black p-1 text-white pr-3 pl-3 hover:bg-opacity-80 drop-shadow-lg"
-            onClick={handleFormSubmit}
-          >
-            생성 하기
-          </button>
+          <div className="w-full mb-3">
+            <p className="mb-2">워크스페이스 이름</p>
+            <input
+              type="text"
+              className="h-10 w-full focus:outline-none max-w-md rounded-md border-2 border-black p-5"
+              placeholder="이름을 정해주세요."
+              value={workspaceName}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div className="w-full mb-5">
+            <p className="mb-2">소개</p>
+            <input
+              type="text"
+              className="h-10 w-full focus:outline-none max-w-md rounded-md border-2 border-black p-5"
+              placeholder="소개글을 작성해주세요."
+              value={workspaceDescription}
+              onChange={handleDescriptionChange}
+            />
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex justify-center items-center">
+              <p className="mr-2">로고 업로드</p>
+              <input
+                type="file"
+                onChange={handleIconChange}
+                className="hidden"
+                id="icon-upload"
+              />
+              <label htmlFor="icon-upload">
+                <img
+                  className="w-6 cursor-pointer"
+                  alt="업로드아이콘"
+                  src={workspaceIcon}
+                />
+              </label>
+            </div>
+            <button
+              type="button"
+              className={`min-w-20 rounded-md bg-black p-1 text-white pr-3 pl-3 hover:bg-opacity-80 drop-shadow-lg  ${
+                !workspaceName && "opacity-50 cursor-not-allowed"
+              }`}
+              onClick={handleFormSubmit}
+              disabled={!workspaceName}
+            >
+              생성 하기
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {isPreviewOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-5 rounded-lg max-w-md">
+            <h2 className="text-xl mb-4">이대로 진행하시겠습니까?</h2>
+            <div className="flex flex-col items-center justify-self-auto p-5">
+              <div className="flex items-center justify-center w-[80px] h-[80px] bg-gray bg-opacity-10 rounded-3xl">
+                <img src={workspaceIcon} alt={workspaceName} />
+              </div>
+              <p className="text-xs font-semibold mt-2">{workspaceName}</p>
+            </div>
+
+            <div className="flex justify-between">
+              <button
+                className="bg-gray-500 px-4 py-2 rounded-md"
+                onClick={handleCancelSubmit}
+              >
+                아니오
+              </button>
+              <button
+                className="bg-blue-500 px-4 py-2 rounded-md"
+                onClick={handleConfirmSubmit}
+              >
+                네
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
