4af069bc927865966e808ce15a9473d6c307ce34
TaekyungAn <atk0426@naver.com>
2024-07-18 20:51:33 +0900

    Add: 404페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import FirstPage from "./pages/FirstPage";
+import WorkSpace from "./pages/WorkSpace";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/firstpage" element={<FirstPage />} />
+        <Route path="/workspace" element={<WorkSpace />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
