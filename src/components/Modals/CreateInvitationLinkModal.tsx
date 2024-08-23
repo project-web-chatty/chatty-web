2b13a7a6f98103b1c67ffc412b75d875d5823c31
kim seung hyun <skim@uroom.io>
2024-08-23 17:28:16 +0900

    Feat : Add channel code copy feature & update CSS

--- src/components/Modals/CreateInvitationLinkModal.tsx
+++ src/components/Modals/CreateInvitationLinkModal.tsx
@@ -1,7 +1,9 @@
-import LinkLogo from "../../assets/icon/icon_link.png";
-import CopyLogo from "../../assets/icon/icon_copy.png";
 import { useEffect, useState } from "react";
 import { getWorkspaceInvitationLink } from "../../api/workspace/WorkSpaceAPI";
+import IconLink from "../../assets/icon/icon_link.png";
+import IconCopy from "../../assets/icon/icon_copy.png";
+import IconClose from "../../assets/icon/icon_close.png";
+import IconCheck from "../../assets/icon/icon_check.png";
 
 interface CreateInvitationLinkProps {
   closeModal: () => void;
@@ -14,13 +16,22 @@ const CreateInvitationLink: React.FC<CreateInvitationLinkProps> = ({
   title,
   workspaceId,
 }) => {
-  const [invitationLink, setInvitationLink] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const [copyIcon, setCopyIcon] = useState(IconCopy);
 
   useEffect(() => {
     getWorkspaceInvitationLink(workspaceId).then(
-      (res: string) => res && setInvitationLink(res)
+      (res: string) => res && setCode(res)
     );
   }, []);
+
+  // 복사 기능 구현 함수
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(code).then(() => {
+      setCopyIcon(IconCheck);
+      setTimeout(() => setCopyIcon(IconCopy), 500);
+    });
+  };
 
   return (
     <>
@@ -29,31 +40,40 @@ const CreateInvitationLink: React.FC<CreateInvitationLinkProps> = ({
         onClick={closeModal}
       >
         <div
-          className="w-96 bg-white p-8 rounded shadow-lg relative"
+          className="w-[540px] h-[290px] bg-white p-8 rounded shadow-lg relative"
           onClick={(e) => e.stopPropagation()}
         >
           <div className="flex justify-between items-center">
-            <p className="text-xl font-bold">{title}</p>
+            <p className="text-3xl font-bold">{title}</p>
             <button
               className="text-gray-500 hover:text-gray-700"
               onClick={closeModal}
             >
-              &times;
+              <img src={IconClose} alt="" className="w-6 h-6" />
             </button>
           </div>
-          <div className="py-5">
-            <p className="text-sm">초대링크</p>
-            <div className="flex w-full items-center mt-2">
+          <div className="mt-[40px]">
+            <p className="text-base mb-[8px]">초대링크</p>
+            <div className="flex w-full items-center ">
               <div className="flex border-2 border-black w-full p-2 rounded-md items-center mr-3">
                 <input
                   type="text"
                   className="w-full text-sm focus:outline-none"
                   placeholder="초대링크를 입력해주세요."
-                  value={invitationLink}
+                  value={code}
+                  readOnly
                 />
-                <img className="w-3 h-3" src={LinkLogo} alt="" />
+                <img className="w-4 h-4" src={IconLink} alt="" />
               </div>
-              <img className="w-3 h-3" src={CopyLogo} alt="" />
+              <div>
+                {/* {copyIcon !== CheckIcon && <span>복사 완료!</span>} */}
+                <img
+                  className="w-4 h-4 cursor-pointer"
+                  onClick={handleCopyClick}
+                  src={copyIcon}
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
