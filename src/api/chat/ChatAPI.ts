2d8c362c796356a274c29b7a48402c942f46cc71
kim seung hyun <skim@uroom.io>
2024-09-23 15:52:04 +0900

    Fix : chat apis

--- src/api/chat/ChatAPI.ts
+++ src/api/chat/ChatAPI.ts
@@ -1,5 +1,34 @@
 import { Get } from "../util/apiUtils";
 
-const chatService = {};
+const chatService = {
+  getMessages: async (channelId: number) => {
+    const response = await Get(`chat/${channelId}`);
 
-export const {} = chatService;
+    return response.data.result;
+  },
+
+  searchLastReadMessageId: async (channelId: number) => {
+    const response = await Get(`chat/${channelId}/read/id`);
+
+    return response.data.result;
+  },
+
+  getLastMessage: async (channelId: number) => {
+    const response = await Get(`chat/${channelId}/last`);
+
+    return response.data.result;
+  },
+
+  getUnreadMessageCount: async (channelId: number) => {
+    const response = await Get(`chat/${channelId}/count`);
+
+    return response.data.result;
+  },
+};
+
+export const {
+  getMessages,
+  searchLastReadMessageId,
+  getLastMessage,
+  getUnreadMessageCount,
+} = chatService;
